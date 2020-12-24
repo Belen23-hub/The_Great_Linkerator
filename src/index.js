cf8a4cd381ca2fe827ed855f315b5722cf05703b
Belén menganita <belenbahmerrada@gmail.com>
2020-12-23 19:30:22 -0600

    starting without css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from './components/app'
+import App from './components/App'
 
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("app")
-);
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App></App>, mountNode)
