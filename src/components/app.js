e3090a3878dda443597214c59fbb375075ed6fdb
Belén menganita <belenbahmerrada@gmail.com>
2020-12-31 15:56:17 -0600

    get links and create new link working. New dropdown for edit and delete.

--- src/components/app.js
+++ src/components/app.js
@@ -3,46 +3,16 @@ import Searchbar from './Searchbar'
 import Links from './Links'
 import "../index.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { getLinks } from '../api/index'
-// import CreateNewLink from "./CreateNewLink"
-import {deleteLink} from "../api/index"
-import {editLink} from "../api/index"
+
 
 
 const App = () => {
 
-    // const [linkLists, setLinksLists] = useState([])
-
-
-    // useEffect(async () => {
-    //     setLinksLists([])
-    //     getLinks().then(setLinksLists, console.error)
-    // }, [])
-
     return (
         <div>
-    
-        {/* {linkLists.map((link, index) => {
-            return (
-            <div key={index}>
-                <h1>{link.link}</h1>
-                <p>{link.comment}</p>
-                <p>{link.clickCount}</p>
-                <button onClick = {()=>{
-
-                deleteLink(link.id)
-
-                }}>delete</button>
-                <button onClick = {()=>{
-
-                }}>edit</button>
-            </div>
-        ) */}
-        {/* })} */}
         <h1>The Great Linkerator</h1>
         <h2>The ONLY solution for indexing urls</h2>
         <Searchbar />
-        {/* <CreateNewLink /> */}
         <Links />
         
     </div>
